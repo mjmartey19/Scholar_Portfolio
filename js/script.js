92594d3e9ab86dcb8b7cb603f8455cf134269cbe
mjmartey19 <89717401+mjmartey19@users.noreply.github.com>
2022-04-18 12:58:59 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,3 +31,4 @@ window.onscroll = () =>{
    });
 
 }
+
---
+++ js/typed_script.js
@@ -0,0 +1,6 @@
+const typed = new Typed(".typed",{
+    strings: ["web Developer","Web Designer","Graphic Designer","UI Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+})
